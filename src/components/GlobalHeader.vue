84c5f63d95ef5fcfcf34e2a96427a4d163386a96
fanshuaiyao <1345822123@qq.com>
2024-11-16 15:26:27 +0800

    前端初始化

---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,91 @@
+<template>
+  <a-row id="globalHeader" align="center" :wrap="false">
+    <a-col flex="auto">
+      <a-menu
+        mode="horizontal"
+        :selected-keys="selectedKeys"
+        @menu-item-click="doMenuClick"
+      >
+        <a-menu-item
+          key="0"
+          :style="{ padding: 0, marginRight: '38px' }"
+          disabled
+        >
+          <div class="title-tar">
+            <img class="logo" src="../assets/cugb.png" />
+            <div class="title">CUGB oj</div>
+          </div>
+        </a-menu-item>
+
+        <a-menu-item v-for="item in visibleRoutes" :key="item.path">
+          {{ item.name }}
+        </a-menu-item>
+      </a-menu>
+    </a-col>
+    <a-col flex="100px">
+      <div>{{ store.state.user?.loginUser?.userName ?? "未登录" }}</div>
+    </a-col>
+  </a-row>
+</template>
+
+<script setup lang="ts">
+import { routes } from "@/router/routes";
+import { useRoute, useRouter } from "vue-router";
+import { computed, ref } from "vue";
+import { useStore } from "vuex";
+import checkAccess from "@/access/checkAccess";
+import AccessEnum from "@/access/accessEnum";
+
+const router = useRouter();
+const store = useStore();
+const loginUser = store.state.user.loginUser;
+
+const visibleRoutes = computed(() => {
+  return routes.filter((item, index) => {
+    if (item.meta?.hideInMenu) {
+      return false;
+    }
+    // 根据权限过滤菜单
+    if (
+      !checkAccess(store.state.user.loginUser, item?.meta?.access as string)
+    ) {
+      return false;
+    }
+    return true;
+  });
+});
+
+// 默认主页
+const selectedKeys = ref(["/"]);
+
+const doMenuClick = (key: string) => {
+  router.push({
+    path: key,
+  });
+};
+router.afterEach((to, from, failure) => {
+  selectedKeys.value = [to.path];
+});
+
+setTimeout(() => {
+  store.dispatch("user/getLoginUser", {
+    userName: "fansy",
+    userRole: AccessEnum.ADMIN,
+  });
+}, 3000);
+</script>
+<style>
+.title-tar {
+  display: flex;
+  align-items: center;
+}
+
+.logo {
+  height: 48px;
+}
+
+.title {
+  color: #444;
+  margin-left: 16px;
+}
+</style>
